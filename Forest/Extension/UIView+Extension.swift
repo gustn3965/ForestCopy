494d2833374f67f7ffdad2e5c8a9dcfff31dfd0a
gustn3965 <gustn3965@gmail.com>
2020-10-31 18:57:47 +0900

    완료

---
+++ Forest/Extension/UIView+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UIView+Extension.swift
+//  Forest
+//
+//  Created by hyunsu on 2020/10/31.
+//
+
+import UIKit
+
+extension UIView {
+    func setRound() {
+        self.layer.cornerRadius = 10
+    }
+}
